9a5fe9812244b966524bbbce7d6430d06133d4ca
Shkyn <randel.vaar@voco.ee>
2025-01-13 13:22:37 +0200

    ylesanne 2 backend index.js uuendamine

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs/promises");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,8 +17,18 @@ app.use((req, res, next) => {
 });
 
 app.get("/meals", async (req, res) => {
-  const meals = "[]" // data should be read from file
-  res.json(JSON.parse(meals));
+  try {
+    // Read the meals.json file
+    const filePath = path.join(__dirname, "data", "meals.json");
+    const data = await fs.readFile(filePath, "utf-8");
+    const meals = JSON.parse(data);
+
+    // Send the meals data as a JSON response
+    res.json(meals);
+  } catch (error) {
+    console.error("Error reading meals data:", error);
+    res.status(500).json({ message: "Failed to load meals data" });
+  }
 });
 
 app.use((req, res) => {
@@ -29,4 +39,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+  console.log("Backend server is running on port 3001");
+});
