a4a0e49a038d23dba71bae36c3318daf686fe9c0
Shkyn <randel.vaar@voco.ee>
2025-01-13 13:34:28 +0200

    valuutad eurodes Mealitem.js lisatud

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -3,8 +3,11 @@ import React from 'react';
 const MealItem = (props) => {
   const { meal } = props;
 
-  // Convert the price from a string to a number
-  const price = parseFloat(meal.price).toFixed(2);
+
+  const formattedPrice = new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(meal.price);
 
   return (
     <li className="meal-item">
@@ -12,7 +15,7 @@ const MealItem = (props) => {
         <img src={require(`../assets/${meal.image}`)} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">${price}</p>
+          <p className="meal-item-price">{formattedPrice}</p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
